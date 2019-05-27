214a18af97fc92d62a137087f6f2599639a8ec9f
Zack Bunn <zrb6578@gmail.com>
2019-05-27 13:51:26 -0500

    expiry time

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
 process.env.TZ = 'UTC';
 process.env.NODE_ENV = 'test';
 process.env.JWT_SECRET = 'test-jwt-secret';
+process.env.JWT_EXPIRY = '3m';
+
+process.env.TEST_DB_URL =
+  process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/thingful';
 
 require('dotenv').config();
 const { expect } = require('chai');
